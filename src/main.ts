5c0a1de7f4aa953da73625bd0f071bdf30583fe4
mask <617473294@qq.com>
2023-04-24 15:55:42 +0800

    style(style): :lipstick: 添加reset.css 和 切换动画样式

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { createApp } from 'vue';
 import 'uno.css';
+import '@/styles/index.scss';
 
 import { createHead } from '@vueuse/head';
-import App from './App.vue';
-import AppLoading from '@/components/Common/AppLoading.vue';
 import { setupStore } from '@/store';
 // import { setupI18n } from '@/locales';
 // import { router, setupRouter } from '@/router';
 import { worker } from '@/api/mocks/browser';
 // import { setupRouterGuard } from '@/router/guard';
+import App from './App.vue';
+import AppLoading from '@/components/Common/AppLoading.vue';
 
 if (process.env.NODE_ENV === 'development') {
   worker.start({
