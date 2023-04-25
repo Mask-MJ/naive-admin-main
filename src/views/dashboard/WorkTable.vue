cf86ade9a4ec1f689fdcc2e8522b9422e1bfedf9
mask <617473294@qq.com>
2023-04-25 11:02:38 +0800

    feat(views): :sparkles: 新增登录页面和工作台页面

---
+++ src/views/dashboard/WorkTable.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts" name="dashboard-WorkTable">
+  const a = ref('');
+  const routerStore = useRouterStore();
+  const menuOptions = computed(() => routerStore.menuList);
+  // onMounted(() => {
+  //   console.log('menuOptions', menuOptions.value)
+  // })
+</script>
+
+<template>
+  <div>
+    <n-input v-model:value="a" />
+    <n-menu :options="menuOptions" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  const userStore = useUserStore();
+
+  async function login() {
+    // await loginApi({
+    //   username: 'admin',
+    //   password: 'admin123',
+    //   code: '1',
+    //   uuid: 'e34b77db035e4fa3a367166d0f341956',
+    // })
+    await userStore.login({
+      username: 'admin',
+      password: 'admin123',
+      code: '1',
+      uuid: 'e34b77db035e4fa3a367166d0f341956',
+    });
+  }
+</script>
+
+<template>
+  <div class="relative h-full bg-black">
+    <n-button type="primary" @click="login"> login </n-button>
+  </div>
+</template>
+
+<style scoped></style>
+
+<route lang="yaml">
+meta:
+  layout: login
+  noAuth: true
+</route>
