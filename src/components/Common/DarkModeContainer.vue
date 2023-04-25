061b4888e111c41fd6cb79d53543155093c2ef27
mask <617473294@qq.com>
2023-04-25 11:35:27 +0800

    feat(components): :sparkles: 新增部分公共模块
    暗黑模式盒子，切换暗黑模式按钮，hover组件封装

---
+++ src/components/Common/DarkModeContainer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts" name="DarkModeContainer">
+  interface Props {
+    inverted?: boolean;
+  }
+
+  withDefaults(defineProps<Props>(), {
+    inverted: false,
+  });
+</script>
+
+<template>
+  <div
+    class="dark:bg-[#18181c] dark:text-white dark:text-opacity-82 transition-all duration-300 ease-in-out"
+    :class="inverted ? 'bg-[#001428] text-white' : 'bg-white text-[#333639]'"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Common/DarkModeSwitch.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts" name="DarkModeSwitch">
+  import { computed } from 'vue';
+
+  interface Props {
+    /** 暗黑模式 */
+    dark?: boolean;
+  }
+
+  const props = withDefaults(defineProps<Props>(), {
+    dark: false,
+  });
+
+  const emit = defineEmits<Emits>();
+
+  interface Emits {
+    (e: 'update:dark', darkMode: boolean): void;
+  }
+
+  const darkMode = computed({
+    get() {
+      return props.dark;
+    },
+    set(newValue: boolean) {
+      emit('update:dark', newValue);
+    },
+  });
+
+  function handleSwitch() {
+    darkMode.value = !darkMode.value;
+  }
+</script>
+
+<template>
+  <div class="flex-center text-18px cursor-pointer" @click="handleSwitch">
+    <i v-if="darkMode" class="i-line-md:moon" />
+    <i v-else class="i-line-md:sunny-outline" />
+  </div>
+</template>
+
+<style scoped></style>
