a4b2224036f3a8dfd4ea2f9d1fde5aa5842f24eb
mask <617473294@qq.com>
2023-05-25 11:54:33 +0800

    fix(components/form): :bug: 修复点击重置按钮没有刷新表格bug

--- src/components/Form/hooks/useFormEvents.ts
+++ src/components/Form/hooks/useFormEvents.ts
@@ -37,7 +37,6 @@ export function useFormEvents({
   // 重置表单值
   const resetPaths = async () => {
     const resetFunc = getProps.value.formAction!.resetButtonOptions!.resetFunc;
-    const submitOnReset = getProps.value.formAction!.submitButtonOptions!.submitOnReset;
     // 如果有自定义重置函数 执行自定义重置函数
     resetFunc && isFunction(resetFunc) && (await resetFunc());
     // 遍历 formModel 把值置为 defaultValue 或 ''
@@ -50,8 +49,7 @@ export function useFormEvents({
     // 清空校验
     nextTick(() => restoreValidation());
     emits('reset', toRaw(formModel));
-    // 如果有设置清空提交查询, 执行查询函数 (例如清空 Table 的筛选 form 重置 Table 数据)
-    submitOnReset && handleSubmit();
+    handleSubmit();
   };
   // 表单提交
   const handleSubmit = async (e?: Event): Promise<void> => {
