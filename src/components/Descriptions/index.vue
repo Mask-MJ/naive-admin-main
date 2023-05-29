0209d11da3732bfadd7824003032fa717cd09555
mask <617473294@qq.com>
2023-05-29 13:44:40 +0800

    feat(components/descriptions): :sparkles: 封装描述组件

---
+++ src/components/Descriptions/index.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts" name="Description">
+  import type { DescItem, DescriptionProps, DescInstance } from './types';
+
+  import { get, isFunction } from 'lodash-es';
+
+  const attrs = useAttrs();
+  const emits = defineEmits(['register']);
+  const props = defineProps({
+    schema: { type: Array as PropType<DescItem[]>, default: () => [] },
+    data: { type: Object },
+    title: { type: String, default: '' },
+    labelPlacement: { type: String, default: 'left' },
+    bordered: { type: Boolean, default: false },
+  });
+
+  const propsRef = ref<Partial<DescriptionProps> | null>(null);
+
+  const getMergeProps = computed(() => ({ ...props, ...unref(propsRef) } as DescriptionProps));
+  const getDescriptionsProps = computed(
+    () => ({ ...unref(attrs), ...unref(getMergeProps) } as DescriptionProps),
+  );
+
+  const getItemProps = computed(() => (item: DescItem) => {
+    const { show, span, label } = item;
+    return { show, span, label };
+  });
+
+  const renderItem = computed(() => (item: DescItem) => {
+    const { render, path } = item;
+    const _data = unref(getMergeProps)?.data;
+    if (!_data) return null;
+    const getPath = get(_data, path);
+    if (getPath && !toRefs(_data).hasOwnProperty(path)) {
+      return isFunction(render) ? render('', _data) : '';
+    }
+    return isFunction(render) ? render(getPath, _data) : getPath ?? '';
+  });
+
+  const setDescProps = (descProps: Partial<DescriptionProps>) => {
+    propsRef.value = { ...(unref(propsRef) as Recordable), ...descProps } as Recordable;
+  };
+
+  const methods: DescInstance = {
+    setDescProps,
+  };
+
+  emits('register', methods);
+</script>
+
+<template>
+  <NDescriptions v-bind="getDescriptionsProps">
+    <n-descriptions-item v-for="item in schema" :key="item.path" v-bind="getItemProps(item)">
+      {{ renderItem(item) }}
+    </n-descriptions-item>
+  </NDescriptions>
+</template>
+
+<style scoped></style>
---
+++ src/components/Descriptions/types.ts
@@ -0,0 +1,24 @@
+import type { DescriptionsProps, DescriptionItemProps } from 'naive-ui';
+
+export type DescItem = DescriptionItemProps & {
+  path: string;
+  show?: (...arg: any) => boolean;
+  // render
+  render?: (
+    val: any,
+    data: Recordable,
+  ) => VNode | undefined | JSX.Element | Element | string | number;
+};
+
+export interface DescriptionProps extends DescriptionsProps {
+  schema: DescItem[];
+  data: Recordable;
+}
+
+export interface DescInstance {
+  setDescProps(descProps: Partial<DescriptionProps>): void;
+}
+
+export type Register = (descInstance: DescInstance) => void;
+
+export type UseDescReturnType = [Register, DescInstance];
